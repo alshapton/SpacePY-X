c78079506ae9684776d1b94ce320b3eaaecbb6e9
Andrew Shapton <Andrew.Shapton@dvla.gov.uk>
2019-08-06 15:12:59 +0100

    realigned the launches and rockets wrapper to use utilities (therefor code is tidier)

--- src/spacexpython/launches.py
+++ src/spacexpython/launches.py
@@ -1,25 +1,21 @@
 import requests
 import urldata
+import utils
 
 
 
 def launches():
     requestUrl = urldata.Domain.main + urldata.Domain.main_launches
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def latest():
     requestUrl = urldata.Domain.main + urldata.Domain.latest_launches
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def next():
     requestUrl = urldata.Domain.main + urldata.Domain.next_launches
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def upcoming():
     requestUrl = urldata.Domain.main + urldata.Domain.upcoming_launches
-    return makeRequest(requestUrl)
-
-def makeRequest(requestUrl):
-    url_response = requests.get(url=str(requestUrl), timeout=1)
-    response = url_response.json()
-    return response
+    return utils.makeRequest(requestUrl)
--- src/spacexpython/rockets.py
+++ src/spacexpython/rockets.py
@@ -1,28 +1,24 @@
 import requests
 import urldata
+import utils
 
 
 def rockets():
     requestUrl = urldata.Domain.main + urldata.Domain.main_rockets
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def falcon1():
     requestUrl = urldata.Domain.main + urldata.Domain.falcon_1
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def falcon9():
     requestUrl = urldata.Domain.main + urldata.Domain.falcon_9
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def falconHeavy():
     requestUrl = urldata.Domain.main + urldata.Domain.falcon_heavy
-    return makeRequest(requestUrl)
+    return utils.makeRequest(requestUrl)
 
 def bfr():
     requestUrl = urldata.Domain.main + urldata.Domain.big_falcon_rocket
-    return makeRequest(requestUrl)
-
-def makeRequest(requestUrl):
-    url_response = requests.get(url=str(requestUrl), timeout=1)
-    response = url_response.json()
-    return response
+    return utils.makeRequest(requestUrl)
