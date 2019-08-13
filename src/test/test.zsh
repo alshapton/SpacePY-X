4bf506ab45230b7247747cfd148b36660387507f
Andrew Shapton <Andrew.Shapton@dvla.gov.uk>
2019-08-13 12:39:28 +0100

    Python 2.7/3.7 test infrastructure setup (correction 12)

--- src/test/test.zsh
+++ src/test/test.zsh
@@ -1,6 +1,10 @@
-VERSION=`echo $1 | cut -c1-4`
-if  [$VERSION = '2.7']
+VERSION=`echo $1 | cut -c1-3`
+MAJOR_VERSION=`echo $VERSION | cut -c1-1`
+if  [ $VERSION = '2.7' ]
 then
   pip install future --user
+  MAJOR_VERSION=''
 fi
-python$VERSION -m pytest
+echo "Python       Version: $VERSION \n"
+echo "Python Major Version: $MAJOR_VERSION \n"
+python$MAJOR_VERSION -m pytest
