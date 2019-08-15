7303daf28f1bf0c819a6d806712ed99f7d0155bf
Andrew Shapton <Andrew.Shapton@dvla.gov.uk>
2019-08-15 15:25:21 +0100

    Finished most of the testing code for Rockets, Launches, Info and Capsules

--- src/test/test.zsh
+++ src/test/test.zsh
@@ -1,10 +1,2 @@
-VERSION=`echo $1 | cut -c1-3`
-MAJOR_VERSION=`echo $VERSION | cut -c1-1`
-if  [ $VERSION = '2.7' ]
-then
-  pip install future --user
-  MAJOR_VERSION=''
-fi
-echo "Python       Version: $VERSION \n"
-echo "Python Major Version: $MAJOR_VERSION \n"
-python$MAJOR_VERSION -m pytest
+#!/usr/local/bin/zsh
+python3 -m pytest  -vv 
