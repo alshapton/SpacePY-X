cdad88dc05214a3bebed6f3acbb0eac2d7c3d458
alshapton <alshapton@gmail.com>
2019-09-02 22:45:53 +0100

    Further PEP8 Corrections

--- src/spacexpython/roadster.py
+++ src/spacexpython/roadster.py
@@ -4,4 +4,4 @@ from . import utils
 
 def roadster(timeOut=1):
     requestUrl = urldata.Domain.main + urldata.Domain.main_roadster
-    return utils.makeRequest(requestUrl,timeOut)
+    return utils.makeRequest(requestUrl,timeOut) 
