2329e1da728518e1d00772cafb3b57eecfe77900
alshapton <alshapton@gmail.com>
2019-09-02 22:57:24 +0100

    PEP8 Updates to test_roadster.py

--- src/test/test_roadster.py
+++ src/test/test_roadster.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append('../')
 import pytest
 import os
@@ -23,95 +22,124 @@ END = datetime.now() + timedelta(days=1)
 TOMORROW = END.strftime('%Y-%m-%d %H:%M:%S')
 
 # Base directory for API-sourced data
-BASE='data/roadster/roadster.'
+BASE = 'data/roadster/roadster.'
+
 
 def test_name(setup_module):
     assert json.loads(setup_module)["API_name"] == "Elon Musk\'s Tesla Roadster"
 
+    
 def test_launch_date_utc(setup_module):
     assert json.loads(setup_module)["API_launch_date_utc"] == "2018-02-06T20:45:00.000Z"
 
+    
 def test_launch_date_unix(setup_module):
     assert json.loads(setup_module)["API_launch_date_unix"] == 1517949900
 
+    
 def test_launch_mass_kg(setup_module):
     assert json.loads(setup_module)["API_launch_mass_kg"] == 1350
 
+    
 def test_launch_mass_lbs(setup_module):
     assert json.loads(setup_module)["API_launch_mass_lbs"] == 2976
 
+    
 def test_norad_id(setup_module):
     assert json.loads(setup_module)["API_norad_id"] == 43205
 
+    
 def test_orbit_type(setup_module):
     assert json.loads(setup_module)["API_orbit_type"] == "heliocentric"
 
+    
 def test_wikipedia(setup_module):
     assert json.loads(setup_module)["API_wikipedia"] == "https://en.wikipedia.org/wiki/Elon_Musk%27s_Tesla_Roadster"
 
+    
 def test_details(setup_module):
-    assert json.loads(setup_module)["API_details"] == "Elon Musk's Tesla Roadster is an electric sports car that served as the dummy payload for the February 2018 Falcon Heavy test flight and is now an artificial satellite of the Sun. Starman, a mannequin dressed in a spacesuit, occupies the driver's seat. The car and rocket are products of Tesla and SpaceX. This 2008-model Roadster was previously used by Musk for commuting, and is the only consumer car sent into space."
+    assert json.loads(setup_module)["API_details"] == "Elon Musk's Tesla Roadster is an electric " + \
+    "sports car that served as the dummy payload for the February 2018 Falcon Heavy test flight " + \
+    "and is now an artificial satellite of the Sun. Starman, a mannequin dressed in a spacesuit, " + \
+    "occupies the driver's seat. The car and rocket are products of Tesla and SpaceX. This 2008-model " + \
+    "Roadster was previously used by Musk for commuting, and is the only consumer car sent into space."
 
+    
 def test_mars_distance_km(setup_module):
-    assert minus_percent(1, json.loads(setup_module)["LCL_mars_distance_km"]) <= json.loads(setup_module)[
-        "API_mars_distance_km"] <= plus_percent(1, json.loads(setup_module)["LCL_mars_distance_km"])
+    assert minus_percent(1, json.loads(setup_module)["LCL_mars_distance_km"]) <= \
+                            json.loads(setup_module)["API_mars_distance_km"] <= \
+            plus_percent(1, json.loads(setup_module)["LCL_mars_distance_km"])
 
+    
 def test_mars_distance_mi(setup_module):
-    assert minus_percent(1, json.loads(setup_module)["LCL_mars_distance_mi"]) <= json.loads(setup_module)[
-        "API_mars_distance_mi"] <= plus_percent(1, json.loads(setup_module)["LCL_mars_distance_mi"])
+    assert minus_percent(1, json.loads(setup_module)["LCL_mars_distance_mi"]) <= \
+                            json.loads(setup_module)["API_mars_distance_mi"] <= \
+            plus_percent(1, json.loads(setup_module)["LCL_mars_distance_mi"])
 
+    
 def test_earth_distance_km(setup_module):
     assert minus_percent(1, json.loads(setup_module)["LCL_earth_distance_km"]) <= json.loads(setup_module)[
         "API_earth_distance_km"] <= plus_percent(1, json.loads(setup_module)["LCL_earth_distance_km"])
 
+    
 def test_earth_distance_mi(setup_module):
     assert minus_percent(1, json.loads(setup_module)["LCL_earth_distance_mi"]) <= json.loads(setup_module)[
         "API_earth_distance_mi"] <= plus_percent(1, json.loads(setup_module)["LCL_earth_distance_mi"])
 
+    
 def test_orbital_speed_kph(setup_module):
     assert minus_percent(1, json.loads(setup_module)["LCL_speed_kph"]) <= json.loads(setup_module)[
         "API_speed_kph"] <= plus_percent(1, json.loads(setup_module)["LCL_speed_kph"])
 
+    
 def test_orbital_speed_mph(setup_module):
     assert minus_percent(1, json.loads(setup_module)["LCL_speed_mph"]) <= json.loads(setup_module)[
         "API_speed_mph"] <= plus_percent(1, json.loads(setup_module)["LCL_speed_mph"])
 
+    
 def test_epoch(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_epoch"]) <= json.loads(setup_module)[
         "API_epoch"] <= plus_percent(1, json.loads(setup_module)["LCL_epoch"])
 
+    
 def test_sma(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_sma"]) <= json.loads(setup_module)[
         "API_sma"] <= plus_percent(1, json.loads(setup_module)["LCL_sma"])
 
+    
 def test_ec(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_ec"]) <= json.loads(setup_module)[
         "API_ec"] <= plus_percent(1, json.loads(setup_module)["LCL_ec"])
 
+    
 def test_qr(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_qr"]) <= json.loads(setup_module)[
         "API_qr"] <= plus_percent(1, json.loads(setup_module)["LCL_qr"])
 
+    
 def test_ad(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_ad"]) <= json.loads(setup_module)[
         "API_ad"] <= plus_percent(1, json.loads(setup_module)["LCL_ad"])
 
+    
 def test_om(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_om"]) <= json.loads(setup_module)[
         "API_om"] <= plus_percent(1, json.loads(setup_module)["LCL_om"])
 
+    
 def test_w(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_w"]) <= json.loads(setup_module)[
         "API_w"] <= plus_percent(1, json.loads(setup_module)["LCL_w"])
 
+    
 def test_inc(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_inc"]) <= json.loads(setup_module)[
         "API_inc"] <= plus_percent(1, json.loads(setup_module)["LCL_inc"])
 
+    
 def test_period(setup_module):
     minus_percent(1, json.loads(setup_module)["LCL_period"]) <= json.loads(setup_module)[
         "API_period"] <= plus_percent(1, json.loads(setup_module)["LCL_period"])
-
 
 
 @pytest.fixture(scope='module')
@@ -135,7 +163,6 @@ def setup_module():
     CDATA = CDATA + '"API_wikipedia":"' + str((json.loads(roadster_data)["wikipedia"])) + '",'
     CDATA = CDATA + '"API_details":"' + str((json.loads(roadster_data)["details"])) + '",'
 
-
     mars_distance_from_api_km = (json.loads(roadster_data)["mars_distance_km"])
     mars_distance_from_api_mi = (json.loads(roadster_data)["mars_distance_mi"])
     earth_distance_from_api_km = (json.loads(roadster_data)["earth_distance_km"])
@@ -154,8 +181,6 @@ def setup_module():
     inc_from_api = (json.loads(roadster_data)["inclination"])
     period_from_api = (json.loads(roadster_data)["period_days"])
 
-
-
     orbitURL = "https://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=1&COMMAND='-143205'&CENTER= '500@10'&MAKE_EPHEM= 'YES'" + \
                "&TABLE_TYPE= 'ELEMENTS'&START_TIME= '" + NOW + "'&STOP_TIME= '" + TOMORROW + "'&STEP_SIZE= '1 d'&OUT_UNITS= 'AU-D'" + \
                "&REF_PLANE = 'ECLIPTIC' &REF_SYSTEM = 'J2000' &TP_TYPE = 'ABSOLUTE' &ELEM_LABELS = 'YES' &CSV_FORMAT = 'NO' & OBJ_DATA = 'YES'"
@@ -173,7 +198,6 @@ def setup_module():
                     "&SKIP_DAYLT = 'NO' &EXTRA_PREC = 'NO' &R_T_S_ONLY = 'NO' &REF_SYSTEM = 'J2000' " + \
                     "&CSV_FORMAT = 'NO' &OBJ_DATA = 'YES' &QUANTITIES = '19,20'"
 
-
     # Get the data for the Mars Distance information from JPL Horizons API
     fg = makeHTTP(marsDistURL, 1)
     # Create a new file with the results of the call to the JPL Horizons API for Mars distance
@@ -188,7 +212,6 @@ def setup_module():
     fg = makeHTTP(earthDistURL, 1)
     # Create a new file with the results of the call to the JPL Horizons API for the Earth Distance
     writeFile(BASE + 'earth', fg, 'w')
-
 
     # EPOCH
     sb = ['script_roadster.zsh epoch', 'epoch']
@@ -298,6 +321,7 @@ def setup_module():
     CDATA = CDATA + '"LAST": 0}'
     writeFile('fred',CDATA,"w")
     return CDATA
+
 
 @pytest.fixture(scope='module')
 def teardown_module():
