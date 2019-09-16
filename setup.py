4aaec4936d30eec548e7d3f8c89d1c49e0972078
Andrew Shapton <Andrew.Shapton@dvla.gov.uk>
2019-09-16 10:34:39 +0100

    added beautiful soup library to requirements.txt

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ install_requires = [{
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     'requests'
+    'beautifulsoup4'
     }
 ]
 
