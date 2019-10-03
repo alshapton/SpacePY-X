f37483c377edc1269cf6dd18f7f1e2dcb4daebb8
alshapton <alshapton@gmail.com>
2019-10-03 14:24:38 +0100

    Bumpversion to 'v1.1.0.a5'

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README-pypi.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = 'v1.1.0.alpha4.Post1'
+version = 'v1.1.0.a5'
 
 install_requires = [{
     # List your project dependencies here.
