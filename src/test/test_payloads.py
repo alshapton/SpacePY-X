6e67c5256e01b0595f26660e6759ba97aa0b371e
alshapton <alshapton@gmail.com>
2019-10-05 19:51:43 +0100

    Corrected failing payload test (#68)
    * Corrected failing payload test
    
    * Corrected PEP8 formatting issues

--- src/test/test_payloads.py
+++ src/test/test_payloads.py
@@ -10,20 +10,20 @@ from .tutils import *
 
 def test_payloads():
     payloads_data=''
-    payloads_result=keyOrder(alphaOrder(readJSONFile('payloads/all.json')),'payload_id')
+    payloads_result=keyOrder(alphaOrder(readJSONFile('payloads/all.json')), 'payload_id')
     try:
-        payloads_data = keyOrder(alphaOrder(spacexpython.payloads.payloads('',1)),'payload_id')
+        payloads_data = keyOrder(alphaOrder(spacexpython.payloads.payloads('', 1)), 'payload_id')
     except spacexpython.utils.SpaceXReadTimeOut:
         pytest.xfail("Space/X API Read Timed Out")
         print("Failure on payloads.allpayloads")
     assert payloads_data == payloads_result
-    
-@pytest.mark.skip(reason="Skip for now - need to understand why it fails.")
+
+
 def test_one_payload():
     one_data=''
-    one_result=alphaOrder(readJSONFile('payloads/oneGTO-2.json'))
+    one_result = alphaOrder(readJSONFile('payloads/oneTelkom-4.json'))
     try:
-        one_data = alphaOrder(spacexpython.payloads.one('GTO-2','',1))
+        one_data = alphaOrder(spacexpython.payloads.one('Telkom-4', '', 1))
     except spacexpython.utils.SpaceXReadTimeOut:
         pytest.xfail("Space/X API Read Timed Out")
         print("Failure on payloads.one('GTO-2')")
