3c34f7420500c6a011111e15ae85427c48a39c4d
alshapton <alshapton@gmail.com>
2019-10-05 21:00:30 +0100

    Added support for Starship (#69)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [{
 
 
 setup(name='SpacePY-X',
-    version="v1.1.0",
+    version="v1.1.1a1",
     description="Simple Python wrapper for the SpaceX API",
     long_description=README,
     classifiers=[
