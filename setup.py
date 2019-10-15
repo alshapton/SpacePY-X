de4694642c63dc2041818fa98af3d15f7b3826d0
alshapton <alshapton@gmail.com>
2019-10-15 22:40:57 +0100

    #101 dep bfr (#106)
    * Deprecated references to bfr

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
 install_requires = [{
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     'requests',
     'beautifulsoup4',
     'tinydb'
@@ -18,25 +15,25 @@ install_requires = [{
 
 
 setup(name='SpacePY-X',
-    version="v1.1.2a1",
-    description="Simple Python wrapper for the SpaceX API",
-    long_description=README,
-    classifiers=[
+      version="v1.1.2a1",
+      description="Simple Python wrapper for the SpaceX API",
+      long_description=README,
+      classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    keywords='wrapper api spacex python',
-    author='Andrew Shapton',
-    author_email='alshapton@gmail.com',
-    url='https://github.com/alshapton/SpacePY-X',
-    license='MIT',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
+      ],
+      keywords='wrapper api spacex python',
+      author='Andrew Shapton',
+      author_email='alshapton@gmail.com',
+      url='https://github.com/alshapton/SpacePY-X',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points={
         'console_scripts':
             ['spacex-python=spacexpython']
-    }
-)
+      }
+     )
