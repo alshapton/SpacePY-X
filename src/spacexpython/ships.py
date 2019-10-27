12b8cdc1e61d4c470d1b95d960a6443b39deb8eb
Andrew Shapton <alshapton@gmail.com>
2019-10-27 14:26:57 +0000

    feat(ships.py): introduced ship-specific functions
    OCISLY, JRTI-1, JRTI-2 and ASOG now have their own specific functions which will take the same
    filters that other ship-functions take
    
    fix #103

--- src/spacexpython/ships.py
+++ src/spacexpython/ships.py
@@ -63,3 +63,103 @@ def one(ship='', parameters='', timeOut=1):
     requestUrl = urldata.Domain.main \
         + urldata.Domain.main_ships + "/" + ship
     return utils.makeRequest(requestUrl, timeOut, parameters)
+
+
+def ocisly(parameters='', timeOut=1):
+    """
+    :type parameters: Optional[str]
+    :type timeOut: Optional[int]
+
+    Parameters
+    ----------
+    parameters
+        optional - a JSON document containing valid query modifiers
+    timeOut
+        optional - an integer stating the timeout in seconds
+                    of the REST api call
+
+    Returns
+    -------
+    string
+        a JSON document containing details about the
+        "Of Course I still Love You" ship
+    """
+    utils.validateParameters(parameters, __name__, 'one')
+    requestUrl = urldata.Domain.main \
+        + urldata.Domain.main_ships + "/" + 'OCISLY'
+    return utils.makeRequest(requestUrl, timeOut, parameters)
+
+
+def jrti1(parameters='', timeOut=1):
+    """
+    :type parameters: Optional[str]
+    :type timeOut: Optional[int]
+
+    Parameters
+    ----------
+    parameters
+        optional - a JSON document containing valid query modifiers
+    timeOut
+        optional - an integer stating the timeout in seconds
+                    of the REST api call
+
+    Returns
+    -------
+    string
+        a JSON document containing details about the
+        "Just Read The Instructions" (first iteration) ship
+    """
+    utils.validateParameters(parameters, __name__, 'one')
+    requestUrl = urldata.Domain.main \
+        + urldata.Domain.main_ships + "/" + 'JRTI-1'
+    return utils.makeRequest(requestUrl, timeOut, parameters)
+
+
+def jrti2(parameters='', timeOut=1):
+    """
+    :type parameters: Optional[str]
+    :type timeOut: Optional[int]
+
+    Parameters
+    ----------
+    parameters
+        optional - a JSON document containing valid query modifiers
+    timeOut
+        optional - an integer stating the timeout in seconds
+                    of the REST api call
+
+    Returns
+    -------
+    string
+        a JSON document containing details about the
+        "Just Read The Instructions" (second iteration) ship
+    """
+    utils.validateParameters(parameters, __name__, 'one')
+    requestUrl = urldata.Domain.main \
+        + urldata.Domain.main_ships + "/" + 'JRTI-2'
+    return utils.makeRequest(requestUrl, timeOut, parameters)
+
+
+def asog(parameters='', timeOut=1):
+    """
+    :type parameters: Optional[str]
+    :type timeOut: Optional[int]
+
+    Parameters
+    ----------
+    parameters
+        optional - a JSON document containing valid query modifiers
+    timeOut
+        optional - an integer stating the timeout in seconds
+                    of the REST api call
+
+    Returns
+    -------
+    string
+        a JSON document containing details about the
+        "A Shortfall Of Gravitas"  ship
+    """
+    utils.validateParameters(parameters, __name__, 'one')
+    requestUrl = urldata.Domain.main \
+        + urldata.Domain.main_ships + "/" + 'ASOG'
+    return utils.makeRequest(requestUrl, timeOut, parameters)
